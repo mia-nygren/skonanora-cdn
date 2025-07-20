aa7708de7df006e4a9e98b4d9bc8c9c1ce033ac5
mia-nygren <marianygren88@gmail.com>
2025-07-20 15:53:32 +0200

    Updates

--- cdn/skonanora.js
+++ cdn/skonanora.js
@@ -1,8 +1,4 @@
 // skonanora.js
 // Custom JavaScript for Skona Nora, to be copied to /public/cdn/skonanora.js for CDN use.
 
-console.log('Skona Nora JS loaded from GitHub Pages!');
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed (Skona Nora)');
-});
+console.log('Skona Nora JS loaded from GitHub Pages - testing!');
