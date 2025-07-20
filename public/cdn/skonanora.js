01af85bc2de719d1afdcaa170fe7f414ee79bef2
mia-nygren <marianygren88@gmail.com>
2025-07-20 15:50:29 +0200

    Remove ignored files from repository

--- public/cdn/skonanora.js
+++
@@ -1,8 +0,0 @@
-// skonanora.js
-// Custom JavaScript for Skona Nora, to be copied to /public/cdn/skonanora.js for CDN use.
-
-console.log('Skona Nora JS loaded from GitHub Pages!');
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed (Skona Nora)');
-});
